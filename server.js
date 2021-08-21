6103b95a475e10234c8173f4574d1d4cd42e196f
Julio de la Cruz <jdelacruz94@msn.com>
2021-08-21 12:56:11 +0200

    authentication works

--- server.js
+++ server.js
@@ -5,17 +5,44 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const routes = require('./routes.js');
+const auth = require('./auth');
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+myDB(async client => {
+  const myDataBase = await client.db('boilerplate').collection('users');
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
